fefcee85bf5f95358f50bd528791352ae8d4daf4
woojiahao <woojiahao1234@gmail.com>
2020-03-19 13:27:45 +0800

    remove: unnecessary data type declaration

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -14,9 +14,9 @@ const (
   Root                RepoPath = "tmp"
   DailyReports        RepoPath = "tmp/csse_covid_19_data/csse_covid_19_daily_reports"
   TimeSeries          RepoPath = "tmp/csse_covid_19_data/csse_covid_19_time_series"
-  ConfirmedTimeSeries RepoPath = TimeSeries + "/time_series_19-covid-Confirmed.csv"
-  DeathsTimeSeries    RepoPath = TimeSeries + "/time_series_19-covid-Deaths.csv"
-  RecoveredTimeSeries RepoPath = TimeSeries + "/time_series_19-covid-Recovered.csv"
+  ConfirmedTimeSeries          = TimeSeries + "/time_series_19-covid-Confirmed.csv"
+  DeathsTimeSeries             = TimeSeries + "/time_series_19-covid-Deaths.csv"
+  RecoveredTimeSeries          = TimeSeries + "/time_series_19-covid-Recovered.csv"
 )
 
 func (path RepoPath) AsString() string {
